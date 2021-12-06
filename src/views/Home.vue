2f2bad605a7a1fd36b9236a8c4f2afe95ab39463
Bruno Ervilha <bruno.ervilha@landapp.com.br>
2021-12-06 20:39:18 -0300

    Start project and test api conection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(product, index) in products" :key="index">
+      <p>{{product.title}}</p>
+      <p>{{product.currencyId}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from "axios";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data (){
+    return{
+      apiUrl: 'https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e',
+      products: null,
+    }
+  },
+  created () {
+    this.loadProducts()
+  },
+  methods: {
+    async loadProducts () {
+      try {
+        const res = await axios.get(this.apiUrl)
+        this.products = res.data.products
+        console.log (this.products)
+      } catch (error) {
+        console.error(error)
+      }
+    },
   }
 }
 </script>
